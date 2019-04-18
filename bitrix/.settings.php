68637949774412eff0f96171fea2aeb59b20cb14
Maxim Boldyrev <maxakeheomax@mail.ru>
2019-04-18 04:03:44 +0300

    init

---
+++ bitrix/.settings.php
@@ -0,0 +1,64 @@
+<?php
+return array (
+  'utf_mode' => 
+  array (
+    'value' => false,
+    'readonly' => true,
+  ),
+  'cache_flags' => 
+  array (
+    'value' => 
+    array (
+      'config_options' => 3600.0,
+      'site_domain' => 3600.0,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' => 
+  array (
+    'value' => 
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' => 
+  array (
+    'value' => 
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => NULL,
+    ),
+    'readonly' => false,
+  ),
+  'connections' => 
+  array (
+    'value' => 
+    array (
+      'default' => 
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => 'localhost',
+        'database' => 'rakamaka_bitrix',
+        'login' => 'rakamaka_bitrix',
+        'password' => 'LsyE61S7',
+        'options' => 2.0,
+      ),
+    ),
+    'readonly' => true,
+  ),
+  'crypto' => 
+  array (
+    'value' => 
+    array (
+      'crypto_key' => '6bf8fa05398bd83b39fdf9cd2b1ee7f0',
+    ),
+    'readonly' => true,
+  ),
+);
