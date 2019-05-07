a776de0b74ef81e229d1a3de6dac66079809fc1c
Roman Ekhlakov <eromacheb@gmail.com>
2019-05-07 14:07:51 +0300

    updated banners

--- bitrix/templates/eshop_bootstrap_black/components/bitrix/news.list/banner_trainings/template.php
+++ bitrix/templates/eshop_bootstrap_black/components/bitrix/news.list/banner_trainings/template.php
@@ -19,32 +19,27 @@ $iblock = GetIBlock($arResult['ITEMS'][0]['IBLOCK_ID']);
 $item = $arResult['ITEMS'][ count($arResult['ITEMS'])-1 ];
 $item_properties = CIBlockElement::GetByID($item['ID'])->GetNextElement()->GetProperties();
 $item
-
 ?>
-<!-- <pre>
-	<? 
-	// var_dump( $item_properties )
-	 ?>
-</pre>  -->
+
 <div class="up-block">
-	<div class="up-slider-wrapper">
-		<div class="up-slider owl-theme">
-			<div class="owl-carousel__up-up-slider-item">
-				<div class="owl-carousel__up-slider-item__slider-content">
-					<p class="owl-carousel__up-slider-item__name"><?= $item_properties['START']['VALUE'] ?></p>
-					<p class="owl-carousel__up-slider-item__slider-content__promo-title"><?= $item['NAME']?></p> 
-					<p class="owl-carousel__up-slider-item__slider-content__slogan"><?= $item_properties['DESCRIPTION']['TEXT'] ?> </p>
-					<div class="owl-carousel__up-slider__content_bottom">
-						<div class="owl-carousel__slider-item-button">
-							<span class="owl-carousel__slider-item-button__text">Участвовать</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="img-wrapper">
-		<img src="<?= CFile::GetPath( $item_properties['IMAGE']['VALUE'] ) ?>" alt="">
-	</div>
+    <div class="up-slider-wrapper">
+        <div class="up-slider owl-theme">
+            <div class="owl-carousel__up-up-slider-item">
+                <div class="owl-carousel__up-slider-item__slider-content">
+                    <p class="owl-carousel__up-slider-item__name"><?= $item_properties['START']['VALUE'] ?></p>
+                    <p class="owl-carousel__up-slider-item__slider-content__promo-title"><?= $item['NAME']?></p>
+                    <p class="owl-carousel__up-slider-item__slider-content__slogan"><?= $item_properties['DESCRIPTION']['TEXT'] ?> </p>
+                    <div class="owl-carousel__up-slider__content_bottom">
+                        <div class="owl-carousel__slider-item-button">
+                            <span class="owl-carousel__slider-item-button__text">Участвовать</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="img-wrapper">
+        <img src="<?= CFile::GetPath( $item_properties['IMAGE']['VALUE'] ) ?>" alt="">
+    </div>
 </div>
 
--- trainings/index.php
+++ trainings/index.php
@@ -115,21 +115,3 @@ $APPLICATION->SetTitle("Тренировки");
     )
 );?>
 
-<div class="promo-train-block__slider-item">
-	<div class="promo-train-block__slider-item__slider-content">
-		<p class="promo-train-block__slider-item__slider-content__promo-title">программа тренировок</p> 
-		<p class="promo-train-block__slider-item__slider-content__slogan">При покупке любого оборудования Rakamakafit - <span class="promo-train-block__title_underline-block-up"> 7-дневный доступ
-		к программе в подарок</span> </p>
-		<p class="promo-train-block__slider-item__slider-content__dicription">Тебя ждет от 4 тренировки в неделю, программа питания на каждый день, мотивационные лекции и статьи, удобное отслеживание результатов.</p>
-		<div class="promo-train-block__slider__content_bottom">
-			<div class="promo-train-block__slider-item-button">
-				<span class="promo-train-block__slider-item-button__text">Купить</span>
-			</div>
-			<div class="promo-train-block__slider-item-more">
-				<span class="promo-train-block__slider-item-more__text">Подробнее</span>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
