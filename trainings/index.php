86e203e515458e6ce73f8a7fb82bcc94122f598e
vavan19 <vavan194@gmail.com>
2019-07-03 23:47:28 +0300

    tranings menu

--- trainings/index.php
+++ trainings/index.php
@@ -2,64 +2,24 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тренировки");
 ?>
-
-<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"BottomSlider",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array("", ""),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "5",
-		"IBLOCK_TYPE" => "banner",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "20",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array("", ""),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "SORT",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N"
-	)
-);?>
+<div class="trainings_category_block">
+	<div class="trainings_category_block-item" style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/main-trainings.jpg); background-size: cover">
+		<div class="trainings_category_block-item__title">Программа тренировок</div>
+		<a href="/trainings/programm"><div class="special-offer--block__item-cart"><span class="special-offer--block__item-cart-button__text">перейти</span></div></a>
+	</div>
+	<div class="trainings_category_block-item"style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/main-trainings.jpg); background-size: cover">
+		<div class="trainings_category_block-item__title">Бесплатный марафон</div>
+		<a href="/trainings/free-marafone">	<div class="special-offer--block__item-cart"><span class="special-offer--block__item-cart-button__text">перейти</span></div></a>
+	</div>
+	<div class="trainings_category_block-item"style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/main-trainings.jpg); background-size: cover">
+		<div class="trainings_category_block-item__title">Упражнения</div>
+		<a href="/trainings/exercises">	<div class="special-offer--block__item-cart"><span class="special-offer--block__item-cart-button__text">перейти</span></div></a>
+	</div>
+	<div class="trainings_category_block-item"style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/main-trainings.jpg); background-size: cover">
+		<div class="trainings_category_block-item__title">Статьи</div>
+		<a href="/trainings/articles"><div class="special-offer--block__item-cart"><span class="special-offer--block__item-cart-button__text">перейти</span></div></a>
+	</div>
+</div>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
 
