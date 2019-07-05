be7c8a3058d295754f0d32ff2a0c8be8f0f14453
Иван Ганенко <ivan.ganenko@lan.qbfin.ru>
2019-07-05 18:41:33 +0300

    catalog element prices

--- bitrix/templates/eshop_bootstrap_black/components/bitrix/catalog/catalog_test/bitrix/catalog.element/.default/template.php
+++ bitrix/templates/eshop_bootstrap_black/components/bitrix/catalog/catalog_test/bitrix/catalog.element/.default/template.php
@@ -46,38 +46,8 @@ $this->setFrameMode(true);
 		</div>
 
 		<div class="button-block">
-			<div class="product-block__description__prices">
-				<span class="price">1690 Р</span>
-				<span class="old-price">2380 Р</span>
-			</div>
-			<div class="product-block__description__buttons">
-				<a href="#"><div class="product-block__description__buy-in-click"><span class="product-block__description__buy-in-click__text">купить в 1 клик</span></div></a>
-				<a href="#"><div class="product-block__description__add-to-cart"><span class="product-block__description__add-to-cart__text">добавить в корзину</span></div></a>
-			</div>
-			<p class="product-block__description__credit_link"><a class="how-start-block__help-link" href="#">Купить в рассрочку</a></p>
-		</div>
-	</div>
-	<script>
-		$('.slider-nav-block').slick({
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			focusOnSelect: true,
-			asNavFor: '.slider-product-view'
-		});
 
-		$('.slider-product-view').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			fade: true,
-			arrows: false
-		});
-	</script>
-</div>
-<!-- end of product block -->
-
-
-
-<!-- Свойства -->
+	<!-- Свойства -->
 	<?if(is_array($arResult["OFFERS"]) && !empty($arResult["OFFERS"])):?>
 	<!-- Если есть преддожения -->
 		<?foreach($arResult["OFFERS"] as $arOffer):?>
@@ -87,31 +57,37 @@ $this->setFrameMode(true);
 			<?endforeach?>
 			<!-- Цены -->
 			<?foreach($arOffer["PRICES"] as $code=>$arPrice):?>
-				<?if($arPrice["CAN_ACCESS"]):?>
-					<?=$arResult["CAT_PRICES"][$code]["TITLE"];?>
-					<?if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]):?>
-						<s><?=$arPrice["PRINT_VALUE"]?></s> <?=$arPrice["PRINT_DISCOUNT_VALUE"]?>
-					<?else:?>
-						<?=$arPrice["PRINT_VALUE"]?>
-					<?endif?>
-					</p>
-				<?endif;?>
+				<div class="product-block__description__prices">
+					<?if($arPrice["CAN_ACCESS"]):?>
+						<?if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]):?>
+							<span class="price"><?=$arPrice["PRINT_DISCOUNT_VALUE"]?></span> 
+							<span class="old-price"><?= $arPrice["PRINT_VALUE"] ?></span>
+						<?else:?>
+							<span class="price"><?=$arPrice["PRINT_VALUE"]?></span>
+						<?endif?>					
+					<?endif;?>
+				</div>
 			<?endforeach;?>
 			<!-- Покупка -->
 			<?if($arOffer["CAN_BUY"]):?>
 					<form action="<?=POST_FORM_ACTION_URI?>" method="post" enctype="multipart/form-data" сlass="add_form">
-						<a href="javascript:void(0)" onclick="if (BX('QUANTITY<?= $arOffer['ID'] ?>').value &gt; 1) BX('QUANTITY<?= $arOffer['ID'] ?>').value--;">-</a>
-	                        <input type="text" name="QUANTITY" value="1" id="QUANTITY<?= $arOffer['ID'] ?>"/>
-	                    <a href="javascript:void(0)" onclick="BX('QUANTITY<?= $arOffer['ID'] ?>').value++;">+</a>
-						<input type="text" name="<?echo $arParams["PRODUCT_QUANTITY_VARIABLE"]?>" value="1" size="5">
+						<a href="javascript:void(0)" onclick="if (BX('QUANTITY<?= $arOffer['ID'] ?>').value &gt; 1) BX('QUANTITY<?= $arOffer['ID'] ?>').value--;"></a>
+						<input type="hidden" name="QUANTITY" value="1" id="QUANTITY<?= $arOffer['ID'] ?>"/>
+	                    <a href="javascript:void(0)" onclick="BX('QUANTITY<?= $arOffer['ID'] ?>').value++;"></a>
+						<input type="hidden" name="<?echo $arParams["PRODUCT_QUANTITY_VARIABLE"]?>" value="1" size="5">
 						<input type="hidden" name="<?echo $arParams["ACTION_VARIABLE"]?>" value="BUY">
 						<input type="hidden" name="<?echo $arParams["PRODUCT_ID_VARIABLE"]?>" value="<?echo $arOffer["ID"]?>">
 						<input type="submit" name="<?echo $arParams["ACTION_VARIABLE"]."BUY"?>" value="<?echo GetMessage("CATALOG_BUY")?>">
 						<input type="submit" name="<?echo $arParams["ACTION_VARIABLE"]."ADD2BASKET"?>" value="<?echo GetMessage("CT_BCE_CATALOG_ADD")?>">
+						<div class="product-block__description__buttons">
+						<a href="#"><div class="product-block__description__buy-in-click"><span class="product-block__description__buy-in-click__text">купить в 1 клик</span></div></a>
+						<a href="#"><div class="product-block__description__add-to-cart"><span class="product-block__description__add-to-cart__text">добавить в корзину</span></div></a>
+						
 					</form>
 			<?elseif(count($arResult["CAT_PRICES"]) > 0):?>
 				<?=GetMessage("CATALOG_NOT_AVAILABLE")?>
 			<?endif?>
+			<? break; ?>
 		<?endforeach;?>
 	<?else:?>
 	<!-- Если нет преддожений -->
@@ -133,7 +109,7 @@ $this->setFrameMode(true);
 				<form action="<?=POST_FORM_ACTION_URI?>" method="post" enctype="multipart/form-data" сlass="add_form">
 				<a href="javascript:void(0)" onclick="if (BX('QUANTITY<?= $arElement['ID'] ?>').value &gt; 1) BX('QUANTITY<?= $arElement['ID'] ?>').value--;">-</a>
                         <input type="text" name="QUANTITY" value="1" id="QUANTITY<?= $arElement['ID'] ?>"/>
-                    <a href="javascript:void(0)" onclick="BX('QUANTITY<?= $arElement['ID'] ?>').value++;">+</a>
+                    <a href="javascript:void(0)" onclick="BX('QUANTITY<?= $arElement['ID'] ?>').value++;"></a>
 				<input type="hidden" name="<?echo $arParams["ACTION_VARIABLE"]?>" value="BUY">
 				<input type="hidden" name="<?echo $arParams["PRODUCT_ID_VARIABLE"]?>" value="<?echo $arResult["ID"]?>">
 				<input type="submit" name="<?echo $arParams["ACTION_VARIABLE"]."BUY"?>" value="<?echo GetMessage("CATALOG_BUY")?>">
@@ -148,5 +124,27 @@ $this->setFrameMode(true);
 	<!-- Описание -->
 	<?=$arResult["DETAIL_TEXT"]?>
 
+	</div>
+		<p class="product-block__description__credit_link"><a class="how-start-block__help-link" href="#">Купить в рассрочку</a></p>
+	</div>
+
+	</div>
+	<script>
+		$('.slider-nav-block').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			focusOnSelect: true,
+			asNavFor: '.slider-product-view'
+		});
+
+		$('.slider-product-view').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			fade: true,
+			arrows: false
+		});
+	</script>
+</div>
 
 
+<!-- end of product block -->
