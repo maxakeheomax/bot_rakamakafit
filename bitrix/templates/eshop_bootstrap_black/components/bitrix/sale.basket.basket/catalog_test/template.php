fd24b52b47fadf1d14e4476f00e1dc614761fce5
Иван Ганенко <ivan.ganenko@lan.qbfin.ru>
2019-07-10 16:03:47 +0300

    basket from scratch

---
+++ bitrix/templates/eshop_bootstrap_black/components/bitrix/sale.basket.basket/catalog_test/template.php
@@ -0,0 +1,22 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main;
+use Bitrix\Main\Localization\Loc;
+\Bitrix\Main\UI\Extension::load("ui.fonts.ruble");
+
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @var string $templateFolder
+ * @var string $templateName
+ * @var CMain $APPLICATION
+ * @var CBitrixBasketComponent $component
+ * @var CBitrixComponentTemplate $this
+ * @var array $giftParameters
+ */
+
+
+echo '<pre>'	;
+var_dump($arResult);
+
+ 
