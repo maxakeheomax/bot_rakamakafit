ec6bfe316a46ead21f2b29d0db60a2ddb50ab28c
Иван Ганенко <ivan.ganenko@lan.qbfin.ru>
2019-07-12 17:13:15 +0300

    Images fix

--- trainings/programm/index.php
+++ trainings/programm/index.php
@@ -163,40 +163,64 @@ $APPLICATION->SetTitle("Программа тренировок");
 	</div>
 </div>
 <!-- end of FAQ block -->
-<!-- bottom-slider-block -->
-<div class="bottom-slider-block">
-	<div class="bottom-slider-block__slider-header">
-		<div class="bottom-slider-block__slider-header_top-content">
-			<div class="bottom-slider-block__slider-header__left-block">
-				<p class="bottom-slider-block__slider-header__left-block__slogan">стань частью <span class="promo-train-block__title_underline-block-up">@rakamaka.fit</span></p>
-			<a href="#">	<img class="bottom-slider-block__slider-header__left-block__logo" src="<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-inst.svg" alt=""></a>
-			</div>
-			<div class="bottom-slider-block__slider-header__right-block">
-				<div class="bottom-slider-block__slogan-button-wrapper">
-					<p class="bottom-slider-block__slider-header__right-block__slogan">Фото счастливых клиентов RAKAMAKAFIT
-					и участниц марафона трансформации тела RAKAMAKAFON</p>
-				</div>			
-			</div>
-		</div>
-		<img class="bottom-slider-block____slider-header__right-block_img" src="<?= SITE_TEMPLATE_PATH ?>/assets/before-after.svg" alt="">
-	</div>
-	<div class="bottom-slider-block__slider-footer"></div>
-	<div class="bottom-slider-block__slider-wrapper">
-<div class="slick-slider">
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-1.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-1.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-2.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-1.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-2.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-1.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-2.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-2.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-1.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-				<div style="background: url(<?= SITE_TEMPLATE_PATH ?>/assets/bottom-slider-img-2.jpg); background-size: 50%;background-position: right;background-repeat: no-repeat;"></div>
-			</div>
-		<div class="bottom-slider-nav-buttons"></div>
-	</div>
-</div>
+
+
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"BottomSlider",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array("",""),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "banner",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array("",""),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?>
 
 
 
